a2cc39b4f3e9136d8f976c6f556774b708b958a2
learn4ever <learn4ever@nocontact>
2014-06-08 02:45:07 -0400

    added comments

--- plot3.R
+++ plot3.R
@@ -14,13 +14,23 @@ doPlot3 <- function() {
        xlab="", 
        ylab="Energy Sub Metering", 
        col="Black")
+  # add the second line graph for the second variable
   lines(tbl$DateTime, tbl$Sub_metering_2, 
-        type="l", col="Red")
+        type="l", 
+        col="Red")
+  
+  # add the third line graph for the third variable
   lines(tbl$DateTime, tbl$Sub_metering_3, 
-        type="l", col="Blue")
-  legend("topright", lty=1, lwd=1, 
+        type="l", 
+        col="Blue")
+  
+  # add the legend
+  legend("topright", 
+         lty=1, lwd=1, 
          col=c("Black","Red","Blue"), 
          legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+  
+  #close the device
   dev.off()
 }
 
